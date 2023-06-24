5718b670991ff804bab9be69b1d0f77d3cb9c604
agustin <brunoagustin12@gmail.com>
2023-06-24 17:39:18 -0300

    Primer push
    Hola

---
+++ Tienda-de-Ropa-main/Modelos/Producto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tienda_de_Ropa.Modelos
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+
+        public String Descripcion { get; set; }
+
+        public bool Activo { get; set; }
+        public String Imagen { get; set; }
+
+        public Categoria categoria { get; set; }
+
+        public int Categoria { get; set; }
+
+        public int SubCategoria { get; set; }
+
+        public Producto(int id, string nombre, string descripcion, bool activo, string imagen, int categoria, int subCategoria)
+        {
+            Id = id;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Activo = activo;
+            Imagen = imagen;
+            Categoria = categoria;
+            SubCategoria = subCategoria;
+        }
+
+        public Producto()
+        {
+
+        }
+        public Producto(string nombre, string descripcion, bool activo, string imagen, int categoria, int subCategoria)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Activo = activo;
+            Imagen = imagen;
+            Categoria = categoria;
+            SubCategoria = subCategoria;
+        }
+    }
+}
